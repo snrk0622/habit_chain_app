149bb5f71ba5936593ad2708ef25a0d4f9e6d1eb
snrk0622 <snrk.0622@gmail.com>
2025-01-18 19:16:30 +0900

    ボトムナビゲーションバーをリファクタリング

--- lib/app.dart
+++ lib/app.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'view/home_view.dart';
-import 'view/setting_view.dart';
-import 'constant/app_constant.dart';
+import 'component/layout/layout_with_bottom_navigation_bar.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -10,59 +8,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Habit Chain',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        primaryColor: Colors.green,
         useMaterial3: true,
       ),
-      home: const _BottomNavigationBar(),
+      home: const LayoutWithBottomNavigationBar(),
     );
   }
 }
 
-class _BottomNavigationBar extends StatefulWidget {
-  const _BottomNavigationBar({super.key});
-
-  @override
-  State<_BottomNavigationBar> createState() => _BottomNavigationBarState();
-}
-
-class _BottomNavigationBarState extends State<_BottomNavigationBar> {
-  static const List<Widget> _pages = <Widget>[
-    HomePage(),
-    SettingPage(),
-  ];
-
-  int _selectedIndex = 0;
-
-  void _onTapItem(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(AppConstant.title),
-      ),
-      body: _pages[_selectedIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _selectedIndex,
-        onTap: _onTapItem,
-        items: const <BottomNavigationBarItem> [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.dashboard),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.tune),
-            label: 'Setting',
-          ),
-        ],
-        type: BottomNavigationBarType.fixed,
-      ),
-    );
-  }
-}
---
+++ lib/component/layout/layout_with_bottom_navigation_bar.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+import '../../view/home_view.dart';
+import '../../view/setting_view.dart';
+import '../../constant/app_constant.dart';
+
+class LayoutWithBottomNavigationBar extends StatefulWidget {
+  const LayoutWithBottomNavigationBar({super.key});
+
+  @override
+  State<LayoutWithBottomNavigationBar> createState() => LayoutWithBottomNavigationBarState();
+}
+
+class LayoutWithBottomNavigationBarState extends State<LayoutWithBottomNavigationBar> {
+  static const List<Widget> _pages = <Widget>[
+    HomePage(),
+    SettingPage(),
+  ];
+
+  int _selectedIndex = 0;
+
+  void _onTapItem(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(AppConstant.title),
+      ),
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onTapItem,
+        items: const <BottomNavigationBarItem> [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.grid_view_rounded),
+            label: 'ホーム',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.face),
+            label: 'アカウント',
+          ),
+        ],
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: Theme.of(context).primaryColor,
+        showUnselectedLabels: false,
+        selectedLabelStyle: const TextStyle(fontSize: 10),
+      ),
+    );
+  }
+}
